b66d1cbe46f3873f5ec562e4a8118513420c31a7
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2024-09-11 13:43:52 +0700

    Thay main

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,7 @@
-class Test {
-    public static void main(String[] args){
-        System.out.println("Hello world");
-    }
+public class MainClass 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("Hello, World!"); 
+  }
 }

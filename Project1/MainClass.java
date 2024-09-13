b6f2e7cfa06acb3686192112f457693aa1a8523d
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2024-09-13 13:54:07 +0700

    Tạo xong PJ

--- Project1/MainClass.java
+++ Project1/MainClass.java
@@ -1,7 +1,8 @@
+package Project1
 public class MainClass 
 {
   public static void main(String[] args) 
   {
-    System.out.println("Hello, World!"); 
+    System.out.println("Hello, World!");
   }
 }

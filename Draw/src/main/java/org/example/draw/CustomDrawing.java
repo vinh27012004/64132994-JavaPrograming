f86929e4a737c1f38954914e7ccd6173d00e9ceb
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2024-11-15 16:50:24 +0700

    UPDATE 2

--- Draw/src/main/java/org/example/draw/CustomDrawing.java
+++ Draw/src/main/java/org/example/draw/CustomDrawing.java
@@ -6,7 +6,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
 
 public class CustomDrawing extends Application {
@@ -16,33 +15,33 @@ public class CustomDrawing extends Application {
     Canvas canvas = new Canvas(400, 400);
     GraphicsContext gc = canvas.getGraphicsContext2D();
 
-    // Vẽ tam giác lớn (hình nón)
+    // Vẽ tam giác lớn (hướng xuống)
     gc.setStroke(Color.BLACK);
     gc.setLineWidth(2);
-    gc.strokePolygon(new double[]{100, 300, 200}, new double[]{100, 100, 300}, 3);
+    gc.strokePolygon(new double[]{100, 300, 200}, new double[]{200, 200, 50}, 3);
 
-    // Vẽ hai tam giác nhỏ (tai)
-    gc.strokePolygon(new double[]{130, 160, 145}, new double[]{100, 160, 140}, 3);
-    gc.strokePolygon(new double[]{240, 270, 255}, new double[]{100, 160, 140}, 3);
+    // Vẽ hai tam giác nhỏ (tai) bên trên tam giác lớn
+    gc.strokePolygon(new double[]{150, 170, 160}, new double[]{100, 50, 110}, 3);
+    gc.strokePolygon(new double[]{230, 250, 240}, new double[]{100, 50, 110}, 3);
 
     // Vẽ tam giác nhỏ bên trong (mũi)
     gc.setStroke(Color.RED);
-    gc.strokePolygon(new double[]{180, 220, 200}, new double[]{180, 180, 200}, 3);
+    gc.strokePolygon(new double[]{180, 220, 200}, new double[]{130, 130, 150}, 3);
 
-    // Vẽ thân hình (nửa vòng tròn)
+    // Vẽ thân hình (nửa vòng tròn phía dưới)
     gc.setStroke(Color.BLACK);
-    gc.strokeArc(150, 250, 100, 100, 0, 180, javafx.scene.shape.ArcType.OPEN);
+    gc.strokeArc(150, 200, 100, 100, 180, 180, javafx.scene.shape.ArcType.OPEN);
 
     // Vẽ hai chân tròn và các đường vòng nhỏ bên trong
-    gc.strokeOval(150, 330, 30, 30);
-    gc.strokeOval(220, 330, 30, 30);
+    gc.strokeOval(150, 280, 30, 30);
+    gc.strokeOval(220, 280, 30, 30);
     gc.setStroke(Color.RED);
-    gc.strokeArc(155, 345, 20, 20, 0, -180, javafx.scene.shape.ArcType.OPEN);
-    gc.strokeArc(225, 345, 20, 20, 0, -180, javafx.scene.shape.ArcType.OPEN);
+    gc.strokeArc(155, 295, 20, 20, 0, -180, javafx.scene.shape.ArcType.OPEN);
+    gc.strokeArc(225, 295, 20, 20, 0, -180, javafx.scene.shape.ArcType.OPEN);
 
     Pane root = new Pane(canvas);
     Scene scene = new Scene(root, 400, 400);
-    primaryStage.setTitle("Custom Drawing");
+    primaryStage.setTitle("Correct Drawing");
     primaryStage.setScene(scene);
     primaryStage.show();
   }

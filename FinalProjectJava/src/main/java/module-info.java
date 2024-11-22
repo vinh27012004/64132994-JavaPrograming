8e61c06a59f01879dd702acdb9f28374c7834259
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2024-11-22 13:51:49 +0700

    up project

---
+++ FinalProjectJava/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ntu.vinh.finalprojectjava {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens ntu.vinh.finalprojectjava to javafx.fxml;
+  exports ntu.vinh.finalprojectjava;
+}

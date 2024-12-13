948b65525cb59b501db3efea0d08330bb1ec3fcc
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2024-12-13 13:21:49 +0700

    UPDATE GIAO DIỆN CÂU 1 - CÂU 10

---
+++ TH_Bai3_FlashQuizGUI/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module th.phamnguyentrivinh.th_bai3_flashquizgui {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens th.phamnguyentrivinh.th_bai3_flashquizgui to javafx.fxml;
+  exports th.phamnguyentrivinh.th_bai3_flashquizgui;
+}
